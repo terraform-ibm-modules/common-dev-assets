14886e93222697573be226db70aab47ed547ce5a
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2022-05-16 15:20:10 +0100

    ci: update pre-commit-sweeper.sh for gha support

--- module-assets/ci/pre-commit-sweeper.sh
+++ module-assets/ci/pre-commit-sweeper.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# This script is designed to run as part of CI on PRs opened by renovate.
-# The script will run the pre-commit hook and if there are any doc changes detected, it will commit them to the PR
+# This script is designed to run as part of Travis or Github Actions on PRs opened by renovate.
+# The script will run the pre-commit hook and if there are any changes detected, it will commit them to the PR
 # branch which will trigger a new run of the pipeline.
 
 set -e
@@ -19,21 +19,37 @@ function git_push() {
   git commit -m "doc: committing files modified by the hook"
   git push origin "${TRAVIS_PULL_REQUEST_BRANCH}"
 }
-# When running in Github Actions Set the environment variables with the following syntax
-#   env:
-#      TRAVIS_PULL_REQUEST_BRANCH: ${{ github.head_ref }}
-#      TRAVIS_PULL_REQUEST: ${{ github.event.number }}
+
+# Determine if PR
+IS_PR=false
+if [ "${GITHUB_ACTIONS}" == "true" ]; then
+  # GITHUB_HEAD_REF: This property is only set when the event that triggers a workflow run is either pull_request or pull_request_target
+  if [ -n "${GITHUB_HEAD_REF}" ]; then
+    IS_PR=true
+    BRANCH="${GITHUB_HEAD_REF}"
+  fi
+elif [ "${TRAVIS}" == "true" ]; then
+  # TRAVIS_PULL_REQUEST: The pull request number if the current job is a pull request, “false” if it’s not a pull request.
+  if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    IS_PR=true
+    BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
+  fi
+else
+  echo "Could not determine CI runtime environment. Script only support travis or github actions."
+  exit 1
+fi
+
 # Only run on PRs created by renovate
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]] && [[ "${TRAVIS_PULL_REQUEST_BRANCH}" =~ "renovate" ]]; then
+if [[ ${IS_PR} == true ]] && [[ "${BRANCH}" =~ "renovate" ]]; then
   if ! pre-commit run --all-files; then
     echo "Pre-commit did not return 0 exit code. Checking if any files changes.."
     # Check if hook modified any files
     if ! git diff --exit-code; then
-      echo "Detected file changes - configuring git to push changes to branch: ${TRAVIS_PULL_REQUEST_BRANCH}"
+      echo "Detected file changes - configuring git to push changes to branch: ${BRANCH}"
       # Configure local git
       git_config
       # Checkout to PR branch
-      git checkout "${TRAVIS_PULL_REQUEST_BRANCH}"
+      git checkout "${BRANCH}"
       # Commit and push changes
       git_push
       echo "Changes pushed in a new commit, exiting with exit code 1 - new commit will trigger new pipeline run"
