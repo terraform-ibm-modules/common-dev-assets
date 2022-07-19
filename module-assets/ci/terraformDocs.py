1cb9e91a219fc71d02f736b932e168c877773c3f
Andrej Kocbek <akocbek@ie.ibm.com>
2022-07-19 14:01:29 +0100

    feat: Add support for auto add examples to readme

--- module-assets/ci/terraformDocs.py
+++ module-assets/ci/terraformDocs.py
@@ -1,19 +1,20 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 examples_markdown = "EXAMPLES.md"
 
 # Return title (first line) of README file
 def get_readme_title(readme_file):
-    with open(readme_file,"r") as reader:
+    with open(readme_file, "r") as reader:
         line = reader.readline()
         print(line)
         return line
 
+
 # Return titles of all README files inside examples folder
 def get_readme_titles():
-    readme_titles=[]
-    for readme_file in Path('examples').rglob('README.md'):
+    readme_titles = []
+    for readme_file in Path("examples").rglob("README.md"):
         path = str(readme_file)
         if not (".terraform") in path:
             readme_title = get_readme_title(path)
@@ -23,15 +24,17 @@ def get_readme_titles():
                 readme_titles.append(data)
     return readme_titles
 
+
 def prepare_example_lines(readme_titles, newlines):
     if len(readme_titles) > 0:
         for readme_title in readme_titles:
             for key, value in readme_title.items():
-                prepare_line = "- [" + value +"](" + key + ")\n"
+                prepare_line = "- [" + value + "](" + key + ")\n"
                 newlines.append(prepare_line)
     else:
         prepare_line = "- [Examples](examples)\n"
         newlines.append(prepare_line)
+
 
 def create_examples_markdown(newlines):
     with open(examples_markdown, "w") as writer:
@@ -39,12 +42,15 @@ def create_examples_markdown(newlines):
             for l in newlines:
                 writer.writelines(l)
 
+
 def run_terraform_docs():
     os.system("terraform-docs -c common-dev-assets/module-assets/.tfdocs-config.yml .")
+
 
 def remove_examples_markdown():
     if os.path.exists(examples_markdown):
         os.remove(examples_markdown)
+
 
 def main():
     newlines = []
@@ -53,5 +59,6 @@ def main():
     create_examples_markdown(newlines)
     run_terraform_docs()
     remove_examples_markdown()
+
 
 main()
