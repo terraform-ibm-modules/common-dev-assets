0b92bbe5a4d4476419c3a6f149df6923c902025a
Andrej Kocbek <akocbek@ie.ibm.com>
2022-08-11 12:34:41 +0100

    feat: Incorporate metadata generator tool in github action release pipeline

--- module-assets/ci/terraform-config-inspect.sh
+++ module-assets/ci/terraform-config-inspect.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -e
+
 go get github.com/IBM-Cloud/terraform-config-inspect@v1.0.0-beta0
-$HOME/go/bin/terraform-config-inspect --json > module-metadata.json
+"$HOME"/go/bin/terraform-config-inspect --json > module-metadata.json
