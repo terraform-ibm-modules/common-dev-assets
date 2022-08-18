4385485fdfcdb05454b864f4ad1cf13fcf017345
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-08-18 10:26:00 +0100

    ci: add flag use_catalog_source to run-catalog-onboarding-pipeline.sh (#101)

--- module-assets/ci/run-catalog-onboarding-pipeline.sh
+++ module-assets/ci/run-catalog-onboarding-pipeline.sh
@@ -15,8 +15,9 @@ gitUrl="${7:-github.ibm.com}"
 gitOrg="${8:-GoldenEye}"
 # This is the prefix on the programatic name in the catalog
 prefix="${9:-goldeneye}"
-publishApikeyOverride="${10:-none}"
-validationApikeyOverride="${11:-none}"
+useCatalogSource="${10:-true}"
+publishApikeyOverride="${11:-none}"
+validationApikeyOverride="${12:-none}"
 
 if [ -z ${CATALOG_TEKTON_WEBHOOK_URL+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_URL is unset"; else echo "CATALOG_TEKTON_WEBHOOK_URL is set"; fi
 if [ -z ${CATALOG_TEKTON_WEBHOOK_TOKEN+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_TOKEN is unset"; else echo "CATALOG_TEKTON_WEBHOOK_TOKEN is set"; fi
@@ -29,12 +30,13 @@ echo "$example"
 echo "$gitUrl"
 echo "$gitOrg"
 echo "$prefix"
+echo "$useCatalogSource"
 
 if [ "${publishApikeyOverride}" != "none" ]; then echo "Catalog publish apikey override detected."; fi
 if [ "${validationApikeyOverride}" != "none" ]; then echo "Validation apikey override detected."; fi
 
 echo "generating payload"
-payload=$(jq -c -n --arg repoName "$repoName" --arg catalogID "$catalogID" --arg offeringID "$offeringID" --arg version "$version" --arg target "$target" --arg example "$example" --arg gitUrl "$gitUrl" --arg gitOrg "$gitOrg" --arg prefix "$prefix" '{"repo-name": $repoName, "catalog-id": $catalogID, "offering-id": $offeringID, "version": $version, "target": $target, "example": $example, "git-url": $gitUrl, "git-org": $gitOrg, "prefix": $prefix, "ibmcloud-catalog-api-key-override": $publishApikeyOverride, "ibmcloud-validation-api-key-override": $validationApikeyOverride}')
+payload=$(jq -c -n --arg repoName "$repoName" --arg catalogID "$catalogID" --arg offeringID "$offeringID" --arg version "$version" --arg target "$target" --arg example "$example" --arg gitUrl "$gitUrl" --arg gitOrg "$gitOrg" --arg prefix "$prefix" '{"repo-name": $repoName, "catalog-id": $catalogID, "offering-id": $offeringID, "version": $version, "target": $target, "example": $example, "git-url": $gitUrl, "git-org": $gitOrg, "prefix": $prefix, "use-catalog-source": $useCatalogSource, "ibmcloud-catalog-api-key-override": $publishApikeyOverride, "ibmcloud-validation-api-key-override": $validationApikeyOverride}')
 
 echo "kicking off tekton pipeline"
 curl -X POST \
