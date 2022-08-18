f1d2dcdc064e72249f7c9766bab5307483b34ca5
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-08-18 13:31:49 +0100

    ci: fix syntax in run-catalog-onboarding-pipeline.sh and set -e (#112)

--- module-assets/ci/run-catalog-onboarding-pipeline.sh
+++ module-assets/ci/run-catalog-onboarding-pipeline.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 # This script is designed to run as a part of CI through the semantic release automation
 # This script calls the webhook for the release pipeline in tekton with the parameters needed to onboard a release of a module to the catalog
@@ -19,8 +20,8 @@ useCatalogSource="${10:-true}"
 publishApikeyOverride="${11:-none}"
 validationApikeyOverride="${12:-none}"
 
-if [ -z ${CATALOG_TEKTON_WEBHOOK_URL+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_URL is unset"; else echo "CATALOG_TEKTON_WEBHOOK_URL is set"; fi
-if [ -z ${CATALOG_TEKTON_WEBHOOK_TOKEN+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_TOKEN is unset"; else echo "CATALOG_TEKTON_WEBHOOK_TOKEN is set"; fi
+if [ -z ${CATALOG_TEKTON_WEBHOOK_URL+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_URL is unset"; exit 1; else echo "CATALOG_TEKTON_WEBHOOK_URL is set"; fi
+if [ -z ${CATALOG_TEKTON_WEBHOOK_TOKEN+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_TOKEN is unset"; exit 1; else echo "CATALOG_TEKTON_WEBHOOK_TOKEN is set"; fi
 
 echo "$repoName"
 echo "$catalogID"
@@ -36,7 +37,31 @@ if [ "${publishApikeyOverride}" != "none" ]; then echo "Catalog publish apikey o
 if [ "${validationApikeyOverride}" != "none" ]; then echo "Validation apikey override detected."; fi
 
 echo "generating payload"
-payload=$(jq -c -n --arg repoName "$repoName" --arg catalogID "$catalogID" --arg offeringID "$offeringID" --arg version "$version" --arg target "$target" --arg example "$example" --arg gitUrl "$gitUrl" --arg gitOrg "$gitOrg" --arg prefix "$prefix" '{"repo-name": $repoName, "catalog-id": $catalogID, "offering-id": $offeringID, "version": $version, "target": $target, "example": $example, "git-url": $gitUrl, "git-org": $gitOrg, "prefix": $prefix, "use-catalog-source": $useCatalogSource, "ibmcloud-catalog-api-key-override": $publishApikeyOverride, "ibmcloud-validation-api-key-override": $validationApikeyOverride}')
+payload=$(jq -c -n --arg repoName "$repoName" \
+                   --arg catalogID "$catalogID" \
+                   --arg offeringID "$offeringID" \
+                   --arg version "$version" \
+                   --arg target "$target" \
+                   --arg example "$example" \
+                   --arg gitUrl "$gitUrl" \
+                   --arg gitOrg "$gitOrg" \
+                   --arg prefix "$prefix" \
+                   --arg useCatalogSource "$useCatalogSource" \
+                   --arg publishApikeyOverride "$publishApikeyOverride" \
+                   --arg validationApikeyOverride "$validationApikeyOverride" \
+                   '{"repo-name": $repoName,
+                     "catalog-id": $catalogID,
+                     "offering-id": $offeringID,
+                     "version": $version,
+                     "target": $target,
+                     "example": $example,
+                     "git-url": $gitUrl,
+                     "git-org": $gitOrg,
+                     "prefix": $prefix,
+                     "use-catalog-source": $useCatalogSource,
+                     "ibmcloud-catalog-api-key-override": $publishApikeyOverride,
+                     "ibmcloud-validation-api-key-override": $validationApikeyOverride
+                   }')
 
 echo "kicking off tekton pipeline"
 curl -X POST \
