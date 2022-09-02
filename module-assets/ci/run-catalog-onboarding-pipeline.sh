65c52f384f42025570e6bcc0dc151cc729b1349c
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-09-02 17:26:56 +0100

    Rework run-catalog-onboarding-pipeline.sh (#122)
    * ci: final clean
    
    * ci: final final clean

--- module-assets/ci/run-catalog-onboarding-pipeline.sh
+++ module-assets/ci/run-catalog-onboarding-pipeline.sh
@@ -1,71 +1,205 @@
 #! /bin/bash
-set -e
 
-# This script is designed to run as a part of CI through the semantic release automation
-# This script calls the webhook for the release pipeline in tekton with the parameters needed to onboard a release of a module to the catalog
-# To run, call this script using the 'exec' plugin in your .releaserc, and provide the repo name, catalogID, offeringID, version, example, gitUrl (optional), gitOrg (optional) and prefix as parameters
-# The webhook URL and token associated with the webhook must be provided via environment variables
+set -euo pipefail
 
-repoName="$1"
-catalogID="$2"
-offeringID="$3"
-version="$4"
-target="$5"
-example="${6:-none}"
-gitUrl="${7:-github.ibm.com}"
-gitOrg="${8:-GoldenEye}"
-# This is the prefix on the programatic name in the catalog
-prefix="${9:-goldeneye}"
-useCatalogSource="${10:-true}"
-publishApikeyOverride="${11:-none}"
-validationApikeyOverride="${12:-none}"
+PRG=$(basename -- "${0}")
 
-if [ -z ${CATALOG_TEKTON_WEBHOOK_URL+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_URL is unset"; exit 1; else echo "CATALOG_TEKTON_WEBHOOK_URL is set"; fi
-if [ -z ${CATALOG_TEKTON_WEBHOOK_TOKEN+x} ]; then echo "CATALOG_TEKTON_WEBHOOK_TOKEN is unset"; exit 1; else echo "CATALOG_TEKTON_WEBHOOK_TOKEN is set"; fi
+USAGE="
+usage:	${PRG}
+        [--help]
 
-echo "$repoName"
-echo "$catalogID"
-echo "$offeringID"
-echo "$version"
-echo "$example"
-echo "$gitUrl"
-echo "$gitOrg"
-echo "$prefix"
-echo "$useCatalogSource"
+        Required environment variables:
+        CATALOG_TEKTON_WEBHOOK_URL
+        CATALOG_TEKTON_WEBHOOK_TOKEN
 
-if [ "${publishApikeyOverride}" != "none" ]; then echo "Catalog publish apikey override detected."; fi
-if [ "${validationApikeyOverride}" != "none" ]; then echo "Validation apikey override detected."; fi
+        Optional environment variables:
+        CATALOG_PUBLISH_APIKEY  (If not set, offering will be published to GoldenEye catalog. Requires --use_publish_apikey_override flag to be passed)
+        CATALOG_VALIDATION_APIKEY  (If not set, offering will validated in GoldenEye dev account. Requires --use_valadation_apikey_override flag to be passed)
 
-echo "generating payload"
-payload=$(jq -c -n --arg repoName "$repoName" \
-                   --arg catalogID "$catalogID" \
-                   --arg offeringID "$offeringID" \
-                   --arg version "$version" \
-                   --arg target "$target" \
-                   --arg example "$example" \
-                   --arg gitUrl "$gitUrl" \
-                   --arg gitOrg "$gitOrg" \
-                   --arg prefix "$prefix" \
-                   --arg useCatalogSource "$useCatalogSource" \
-                   --arg publishApikeyOverride "$publishApikeyOverride" \
-                   --arg validationApikeyOverride "$validationApikeyOverride" \
-                   '{"repo-name": $repoName,
-                     "catalog-id": $catalogID,
-                     "offering-id": $offeringID,
-                     "version": $version,
-                     "target": $target,
-                     "example": $example,
-                     "git-url": $gitUrl,
-                     "git-org": $gitOrg,
-                     "prefix": $prefix,
-                     "use-catalog-source": $useCatalogSource,
-                     "ibmcloud-catalog-api-key-override": $publishApikeyOverride,
-                     "ibmcloud-validation-api-key-override": $validationApikeyOverride
-                   }')
+        Required arguments:
+        --catalog_id=<catalog-id>
+        --offering_id=<offering-id>
+        --version=<version>
+        --target=<target>  (ibm, account or public)
 
-echo "kicking off tekton pipeline"
-curl -X POST \
-"$CATALOG_TEKTON_WEBHOOK_URL" \
--H "Content-Type: application/json" \
--H "token: ${CATALOG_TEKTON_WEBHOOK_TOKEN}" \
--d "$payload"
+        Optional arguments:
+        [--destroy-on-failure]  (By default resources will not be destroyed on validation failure to allow to debug. Use this flag to always attempt a destroy)
+        [--use_default_targz]  (Publish / validate using the default tar.gz. If not used catalog.tar.gz will be used)
+        [--use_publish_apikey_override]  (Requires CATALOG_PUBLISH_APIKEY env var to be set)
+        [--use_valadation_apikey_override]  (Requires CATALOG_VALIDATION_APIKEY env var to be set)
+        [--validation_dir_list=<validation-dir-list>]  (If not using ibm_catalog.json, then pass a comma seperated list of directories to validate)
+        [--github_url=<github_url>]  (Defaults to github.ibm.com)
+        [--github_org=<github-org>]  (Defaults to GoldenEye)
+        [--programmatic_name_prefix=<prefix>]  (If not used, no prefix will be added to programmatic name)
+"
+
+# Verify required environment variables are set
+all_exist=true
+env_var_array=( CATALOG_TEKTON_WEBHOOK_URL CATALOG_TEKTON_WEBHOOK_TOKEN )
+set +u
+for var in "${env_var_array[@]}"; do
+  [ -z "${!var}" ] && echo "$var not defined." && all_exist=false
+done
+set -u
+if [ $all_exist == false ]; then
+  echo "One or more required environment variables are not defined. Exiting."
+  exit 1
+fi
+
+# Pre-set macros so nounset doesn't complain
+CATALOG_ID=""
+OFFERING_ID=""
+VERSION=""
+TARGET=""
+VALIDATION_DIR_LIST=""
+GITHUB_URL="github.ibm.com"
+GITHUB_ORG="GoldenEye"
+PROGRAMMATIC_NAME_PREFIX=""
+VALIDATION_JSON_FILENAME="ibm_catalog.json"
+USE_DEFAULT_TARGZ=false
+DESTROY_ON_FAILURE=false
+PUBLISH_APIKEY_OVERRIDE="none"
+VALIDATION_APIKEY_OVERRIDE="none"
+REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
+
+# Loop through all args
+for arg in "$@"; do
+  if [ ${arg} = --use_default_targz ]; then
+    USE_DEFAULT_TARGZ=true
+  elif [ ${arg} = --destroy-on-failure ]; then
+    DESTROY_ON_FAILURE=true
+  elif [ ${arg} = --use_publish_apikey_override ]; then
+    set +u
+    if [ -z "${CATALOG_PUBLISH_APIKEY}" ]; then
+      echo "CATALOG_PUBLISH_APIKEY environment variable must be set when using --use_publish_apikey_override flag"
+      exit 1
+    else
+      PUBLISH_APIKEY_OVERRIDE="${CATALOG_PUBLISH_APIKEY}"
+    fi
+    set -u
+  elif [ ${arg} = --use_valadation_apikey_override ]; then
+    set +u
+    if [ -z "${CATALOG_VALIDATION_APIKEY}" ]; then
+      echo "CATALOG_VALIDATION_APIKEY environment variable must be set when using --use_valadation_apikey_override flag"
+      exit 1
+    else
+      VALIDATION_APIKEY_OVERRIDE="${CATALOG_VALIDATION_APIKEY}"
+    fi
+    set -u
+  else
+    set +e
+    found_match=false
+    if echo "${arg}" | grep -q -e --catalog_id=; then
+      CATALOG_ID=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --offering_id=; then
+      OFFERING_ID=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --version=; then
+      VERSION=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --target=; then
+      TARGET=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --validation_dir_list=; then
+      VALIDATION_DIR_LIST=$(echo ${arg} | awk -F= '{ print $2 }' | sed 's/,/ /g')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --github_url=; then
+      GITHUB_URL=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --github_org=; then
+      GITHUB_ORG=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if echo "${arg}" | grep -q -e --programmatic_name_prefix=; then
+      PROGRAMMATIC_NAME_PREFIX=$(echo "${arg}" | awk -F= '{ print $2 }')
+      found_match=true
+    fi
+    if [ ${found_match} = false ]; then
+      if [ ${arg} != --help ]; then
+        echo "Unknown command line argument:  ${arg}"
+      fi
+      echo "${USAGE}"
+      exit 1
+    fi
+    set -e
+  fi
+done
+
+# Verify values have been passed for required args
+if [ "${CATALOG_ID}" = "" ] || [ "${OFFERING_ID}" = "" ] || [ "${VERSION}" = "" ] || [ "${TARGET}" = "" ]; then
+  echo "Missing catalog_id, offering_id, version, or target definitions"
+  echo "--catalog_id=${CATALOG_ID}"
+  echo "--offering_id=${OFFERING_ID}"
+  echo "--version=${VERSION}"
+  echo "--target=${TARGET}"
+  exit 1
+fi
+
+# Verify target value is only ibm, account or public
+if [ "${TARGET}" != "ibm" ] && [ "${TARGET}" != "account" ] && [ "${TARGET}" != "public" ]; then
+  echo "--target value must be ibm, account or public"
+  exit 1
+fi
+
+# Generate array of validation directories
+if [ "${VALIDATION_DIR_LIST}" == "" ]; then
+  if [ ! -f "${VALIDATION_JSON_FILENAME}" ]; then
+    echo "Could not find required file ibm_catalog.json"
+    echo "Please add this file with required content, or use the --validation_dir_list flag to pass a list of directories to run validation on."
+    exit 1
+  else
+    echo "Parsing details from ${VALIDATION_JSON_FILENAME} .."
+    dir_array=()
+    while IFS='' read -r line; do dir_array+=("$line"); done < <(jq -r '.flavors | .[] | .working_directory' "${VALIDATION_JSON_FILENAME}")
+  fi
+else
+  IFS=', ' read -r -a dir_array <<< "${VALIDATION_DIR_LIST}"
+fi
+
+# Loop through tf directories to run validation on and kick off one pipeline instance per directory
+for validation_dir in "${dir_array[@]}"; do
+  echo "Generating payload for ${validation_dir} .."
+  payload=$(jq -c -n --arg repoName "${REPO_NAME}" \
+                     --arg catalogID "${CATALOG_ID}" \
+                     --arg offeringID "${OFFERING_ID}" \
+                     --arg version "${VERSION}" \
+                     --arg target "${TARGET}" \
+                     --arg dir "${validation_dir}" \
+                     --arg gitUrl "${GITHUB_URL}" \
+                     --arg gitOrg "${GITHUB_ORG}" \
+                     --arg prefix "${PROGRAMMATIC_NAME_PREFIX}" \
+                     --arg useDefaultTargz "${USE_DEFAULT_TARGZ}" \
+                     --arg publishApikeyOverride "${PUBLISH_APIKEY_OVERRIDE}" \
+                     --arg validationApikeyOverride "${VALIDATION_APIKEY_OVERRIDE}" \
+                     --arg destroyOnFailure "${DESTROY_ON_FAILURE}" \
+                     '{"repo-name": $repoName,
+                       "catalog-id": $catalogID,
+                       "offering-id": $offeringID,
+                       "version": $version,
+                       "target": $target,
+                       "validation-working-directory": $dir,
+                       "git-url": $gitUrl,
+                       "git-org": $gitOrg,
+                       "programmatic-name-prefix": $prefix,
+                       "use-default-targz": $useDefaultTargz,
+                       "external-catalog-api-key-override": $publishApikeyOverride,
+                       "external-validation-api-key-override": $validationApikeyOverride,
+                       "destroy-on-failure": $destroyOnFailure
+                     }')
+
+  echo "Kicking off tekton pipeline for ${validation_dir}.."
+  curl -X POST \
+  "$CATALOG_TEKTON_WEBHOOK_URL" \
+  -H "Content-Type: application/json" \
+  -H "token: ${CATALOG_TEKTON_WEBHOOK_TOKEN}" \
+  -d "$payload"
+
+  sleep 5
+done
