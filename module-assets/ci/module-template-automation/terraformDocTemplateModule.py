1cc0207b0d2c187ea6e194740fee23dc4790870e
akocbek <106765658+akocbek@users.noreply.github.com>
2022-09-06 12:32:57 +0100

    feat: Add support to create template module README from template (#115)

---
+++ module-assets/ci/module-template-automation/terraformDocTemplateModule.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+from subprocess import PIPE, Popen
+
+
+def create_tf_input(module_name):
+    print(module_name)
+    file = open("%s.tf" % (module_name), "w+")
+    file.write(
+        """
+        variable "%s" {
+            type = string
+        }
+    """
+        % (module_name)
+    )
+    file.close()
+
+
+def run_terraform_docs():
+    subprocess.run(
+        [
+            "terraform-docs",
+            "-c",
+            "common-dev-assets/module-assets/ci/module-template-automation/.terraform-docs-config-template-module.yaml",
+            ".",
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "terraform-docs",
+            "-c",
+            "common-dev-assets/module-assets/ci/module-template-automation/.terraform-docs-config-template-module-contribution.yaml",
+            ".",
+        ],
+        check=True,
+    )
+
+
+def remove_tf_input(module_name):
+    if os.path.exists("%s.tf" % (module_name)):
+        os.remove("%s.tf" % (module_name))
+
+
+def main():
+    # get repository name
+    my_command = "basename `git config --get remote.origin.url`"
+    proc = Popen(my_command, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = proc.communicate()
+    module_name = output.decode("utf-8").strip().replace(".git", "")
+
+    if proc.returncode != 0:
+        print(error)
+    elif (
+        module_name == "module-template"
+        or module_name == "terraform-ibm-module-template"
+    ):
+        create_tf_input(module_name)
+        run_terraform_docs()
+        remove_tf_input(module_name)
+
+
+main()
