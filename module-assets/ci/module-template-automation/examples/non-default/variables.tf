03b1ec3d28a32b5aebda1f39715baa275f62652c
Andrej Kocbek <akocbek@ie.ibm.com>
2022-09-07 07:33:39 +0100

    feat: Store module template examples in common-dev-assets

--- module-assets/ci/module-template-automation/examples/non-default/variables.tf
+++ module-assets/ci/module-template-automation/examples/non-default/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
-  default     = "goldeneye"
+  default     = "terraform"
 }
 
 variable "resource_group" {
