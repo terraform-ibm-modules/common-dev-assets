f81ba6d543ce87691caed381226baceb8104a544
Andrej Kocbek <akocbek@ie.ibm.com>
2022-09-12 09:50:22 +0100

    feat: store template module tests

--- module-assets/ci/module-template-automation/moduleTemplateBuilder.py
+++ module-assets/ci/module-template-automation/moduleTemplateBuilder.py
@@ -6,15 +6,16 @@ import subprocess
 from subprocess import PIPE, Popen
 
 
-def create_tf_input(module_name):
+def create_tf_input(module_name, module_url):
     file = open("%s.tf" % (module_name), "w+")
     file.write(
         """
         variable "%s" {
             type = string
+            description = "%s"
         }
     """
-        % (module_name)
+        % (module_name, module_url)
     )
     file.close()
 
@@ -38,6 +39,15 @@ def run_terraform_docs():
         ],
         check=True,
     )
+    subprocess.run(
+        [
+            "terraform-docs",
+            "-c",
+            "common-dev-assets/module-assets/ci/module-template-automation/.terraform-docs-config-template-module-tests.yaml",
+            ".",
+        ],
+        check=True,
+    )
 
 
 def copy_common_code():
@@ -47,6 +57,7 @@ def copy_common_code():
     shutil.copytree(
         "ci/module-template-automation/common_code", "./", dirs_exist_ok=True
     )
+    shutil.copytree("ci/module-template-automation/tests", "tests", dirs_exist_ok=True)
 
 
 def remove_tf_input(module_name):
@@ -56,19 +67,38 @@ def remove_tf_input(module_name):
 
 def main():
     # get repository name
-    my_command = "basename `git config --get remote.origin.url`"
-    proc = Popen(my_command, stdout=PIPE, stderr=PIPE, shell=True)
+    get_repository_url_name = "basename `git config --get remote.origin.url`"
+    proc = Popen(get_repository_url_name, stdout=PIPE, stderr=PIPE, shell=True)
     output, error = proc.communicate()
     module_name = output.decode("utf-8").strip().replace(".git", "")
 
     if proc.returncode != 0:
         print(error)
-    elif (
+        return
+
+    # get repository url
+    get_repository_url_command = "git config --get remote.origin.url"
+    proc = Popen(get_repository_url_command, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = proc.communicate()
+    module_url = (
+        output.decode("utf-8")
+        .strip()
+        .replace(".git", "")
+        .replace("git@", "")
+        .replace("https://", "")
+        .replace(":", "/")
+    )
+
+    if proc.returncode != 0:
+        print(error)
+        return
+
+    if (
         module_name == "module-template"
         or module_name == "terraform-ibm-module-template"
     ):
         copy_common_code()
-        create_tf_input(module_name)
+        create_tf_input(module_name, module_url)
         run_terraform_docs()
         remove_tf_input(module_name)
 
---
+++ module-assets/ci/module-template-automation/tests/pr_test.go
@@ -0,0 +1,48 @@
+// Tests in this file are run in the PR pipeline
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+)
+
+// Use existing resource group
+const resourceGroup = "geretain-test-resources"
+const defaultExampleTerraformDir = "examples/default"
+
+func TestRunDefaultExample(t *testing.T) {
+	t.Parallel()
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  defaultExampleTerraformDir,
+		Prefix:        "mod-template",
+		ResourceGroup: resourceGroup,
+	})
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
+
+func TestRunUpgradeExample(t *testing.T) {
+	t.Parallel()
+
+	// TODO: Remove this line after the first merge to master branch is complete to enable upgrade test
+	t.Skip("Skipping upgrade test until initial code is in master branch")
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  defaultExampleTerraformDir,
+		Prefix:        "mod-template-upg",
+		ResourceGroup: resourceGroup,
+	})
+
+	output, err := options.RunTestUpgrade()
+	if !options.UpgradeTestSkipped {
+		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
+	}
+}
