0b18a1ec929c539a456fe9fb542d3f472c54ccbb
Andrej Kocbek <akocbek@ie.ibm.com>
2022-09-12 13:02:52 +0100

    feat: pre-commit-hook to ensure module name is correct in go.mod

--- module-assets/ci/terraformDocGoMod.py
+++ module-assets/ci/terraformDocGoMod.py
@@ -1,83 +1,49 @@
 #!/usr/bin/python
 
-import os
-import subprocess
 from pathlib import Path
 from subprocess import PIPE, Popen
 
 
-def create_tf_input(file_name, module_url):
-    file = open("%s.tf" % (file_name), "w+")
-    file.write(
-        """
-        variable "%s" {
-            type = string
-            description = "%s"
-        }
-    """
-        % (file_name, module_url)
-    )
-    file.close()
-
-
-def run_terraform_docs():
-    subprocess.run(
-        [
-            "terraform-docs",
-            "-c",
-            "common-dev-assets/module-assets/.terraform-docs-config-go-mod.yaml",
-            ".",
-        ],
-        check=True,
-    )
-
-
-def remove_tf_input(file_name):
-    if os.path.exists("%s.tf" % (file_name)):
-        os.remove("%s.tf" % (file_name))
-
-
-# Check if go.mod file already have a pre-commit hook metadata tag. If not, replace the first line with a metadata tag
-def check_go_mod(path):
-
+# Set go.mod file with the correct module repo
+def set_go_mod(path, module_url):
     with open(path, "r") as file:
         lines = file.readlines()
+    if lines:
+        first_line = lines[0]
+        expected_line = "module " + module_url
+        if first_line.strip() != expected_line:
+            lines.insert(0, expected_line + "\n")
+            with open(path, "w") as writer:
+                writer.writelines(lines)
 
-    first_line = lines[0]
-    if first_line.strip() == "// START MODULE NAME HOOK":
+
+# get repository url
+def get_module_url():
+    get_repository_url_command = "git config --get remote.origin.url"
+    proc = Popen(get_repository_url_command, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = proc.communicate()
+    module_url = (
+        output.decode("utf-8")
+        .strip()
+        .replace(".git", "")
+        .replace("git@", "")
+        .replace("https://", "")
+        .replace(":", "/")
+    )
+
+    if proc.returncode != 0:
+        print(error)
         return
     else:
-        lines[0] = "// START MODULE NAME HOOK\n// END MODULE NAME HOOK\n"
-        with open(path, "w") as writer:
-            writer.writelines(lines)
+        return module_url
 
 
 def main():
     go_mod_path = Path("tests/go.mod")
     if go_mod_path.is_file():
-        tf_inputs_temp_file = "tf_inputs_temp_file"
-
-        # get repository url
-        get_repository_url_command = "git config --get remote.origin.url"
-        proc = Popen(get_repository_url_command, stdout=PIPE, stderr=PIPE, shell=True)
-        output, error = proc.communicate()
-        module_url = (
-            output.decode("utf-8")
-            .strip()
-            .replace(".git", "")
-            .replace("git@", "")
-            .replace("https://", "")
-            .replace(":", "/")
-        )
-
-        if proc.returncode != 0:
-            print(error)
-            return
-
-        check_go_mod(go_mod_path)
-        create_tf_input(tf_inputs_temp_file, module_url)
-        run_terraform_docs()
-        remove_tf_input(tf_inputs_temp_file)
+        module_url = get_module_url()
+        if module_url:
+            set_go_mod(go_mod_path, module_url)
 
 
 main()
