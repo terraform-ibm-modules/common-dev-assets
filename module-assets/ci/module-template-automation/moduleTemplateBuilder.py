b18355101eefeaffbe96ea770e01572cff8fa5d9
Andrej Kocbek <akocbek@ie.ibm.com>
2022-09-12 10:21:10 +0100

    feat: store template module tests

--- module-assets/ci/module-template-automation/moduleTemplateBuilder.py
+++ module-assets/ci/module-template-automation/moduleTemplateBuilder.py
@@ -6,16 +6,15 @@ import subprocess
 from subprocess import PIPE, Popen
 
 
-def create_tf_input(module_name, module_url):
+def create_tf_input(module_name):
     file = open("%s.tf" % (module_name), "w+")
     file.write(
         """
         variable "%s" {
             type = string
-            description = "%s"
         }
     """
-        % (module_name, module_url)
+        % (module_name)
     )
     file.close()
 
@@ -74,31 +73,12 @@ def main():
 
     if proc.returncode != 0:
         print(error)
-        return
-
-    # get repository url
-    get_repository_url_command = "git config --get remote.origin.url"
-    proc = Popen(get_repository_url_command, stdout=PIPE, stderr=PIPE, shell=True)
-    output, error = proc.communicate()
-    module_url = (
-        output.decode("utf-8")
-        .strip()
-        .replace(".git", "")
-        .replace("git@", "")
-        .replace("https://", "")
-        .replace(":", "/")
-    )
-
-    if proc.returncode != 0:
-        print(error)
-        return
-
-    if (
+    elif (
         module_name == "module-template"
         or module_name == "terraform-ibm-module-template"
     ):
         copy_common_code()
-        create_tf_input(module_name, module_url)
+        create_tf_input(module_name)
         run_terraform_docs()
         remove_tf_input(module_name)
 
