0f8e5f2c128704fda2d7389897809e6ad9ab1880
Andrej Kocbek <akocbek@ie.ibm.com>
2022-09-16 08:48:26 +0100

    fix: use urlparse for parsing urls

--- module-assets/ci/terraformDocGoMod.py
+++ module-assets/ci/terraformDocGoMod.py
@@ -4,6 +4,7 @@ import re
 import sys
 from pathlib import Path
 from subprocess import PIPE, Popen
+from urllib.parse import urlparse
 
 
 # Set go.mod file with the correct module repo
@@ -31,20 +32,20 @@ def get_module_url():
     get_repository_url_command = "git config --get remote.origin.url"
     proc = Popen(get_repository_url_command, stdout=PIPE, stderr=PIPE, shell=True)
     output, error = proc.communicate()
-    module_url = (
-        output.decode("utf-8")
-        .strip()
-        .replace(".git", "")
-        .replace("git@", "")
-        .replace("https://", "")
-        .replace(":", "/")
-    )
+    full_url = output.decode("utf-8").strip()
 
     if proc.returncode != 0:
         print(error)
         sys.exit(proc.returncode)
+
+    # urlparse can not be used for git urls
+    if full_url.startswith("http"):
+        o = urlparse(full_url)
+        module_url = o.hostname + o.path
     else:
-        return module_url
+        module_url = full_url.replace(".git", "").replace("git@", "").replace(":", "/")
+
+    return module_url
 
 
 def main():
