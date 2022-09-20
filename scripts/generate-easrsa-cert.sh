a090478836fd1d32e4bd4ab1be519dd204744d12
Rajat Agrawal <rajatatibm@gmail.com>
2022-09-20 13:35:29 +0100

    add easyrsa script

---
+++ scripts/generate-easrsa-cert.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Docs: https://cloud.ibm.com/docs/vpc?topic=vpc-client-to-site-authentication
+
+REF_PATH=$1
+
+# Create a new PKI and CA:
+create_pki_and_ca() {
+    echo | ./"${REF_PATH}"/easy-rsa/easyrsa3/easyrsa init-pki
+    echo | ./"${REF_PATH}"/easy-rsa/easyrsa3/easyrsa build-ca nopass
+}
+
+# Generate a VPN server certificate.
+generate_server_cert() {
+    echo "yes" | ./"${REF_PATH}"/easy-rsa/easyrsa3/easyrsa build-server-full vpn-server.vpn.ibm.com nopass
+}
+
+# Generate a VPN client certificate.
+generate_client_cert() {
+    echo "yes" | ./"${REF_PATH}"/easy-rsa/easyrsa3/easyrsa build-client-full client1.vpn.ibm.com nopass
+}
+
+# Cert Path:
+locate_certs() {
+    echo "VPN client public key is generated at path: ""$PWD""/pki/issued/client1.vpn.ibm.com.crt"
+    echo "VPN client private key is generated at path: ""$PWD""/pki/private/client1.vpn.ibm.com.key"
+    # echo $PWD
+    echo "Certificate File: ""$PWD""/pki/issued/vpn-server.vpn.ibm.com.crt"
+    echo "Certificate certificate's private key: ""$PWD""/pki/private/vpn-server.vpn.ibm.com.key"
+    echo "Intermediate certificate File: ""$PWD""/pki/ca.crt"
+}
+
+init() {
+    create_pki_and_ca
+    generate_server_cert
+    generate_client_cert
+    locate_certs
+}
+
+init
