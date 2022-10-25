724e7d0352159df9e4ee19dab92bc15922c06870
Andrej <ak@Andrejs-MacBook-Pro.local>
2022-10-25 09:37:38 +0100

    feat: use ibm provider for module-metadata generator

---
+++ module-assets/ci/terraformConfigInspect.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import glob
+import os
+import sys
+from pathlib import Path
+
+
+def terraform_init():
+    os.system("terraform init")
+
+
+def remove_terraform_folder():
+    os.system("rm -fr .terraform")
+
+
+def get_terraform_provider():
+    for terraform_provider in Path(".terraform").rglob("provider_metadata.json"):
+        return terraform_provider
+
+
+def run_metadata_generator(file_path, terrraform_provider):
+    os.system(
+        "terraform-config-inspect --json > %s --metadata %s --filter-variables"
+        % (file_path, terrraform_provider)
+    )
+
+
+def main():
+    if glob.glob("*.tf"):
+        tf_folder_already_exists = os.path.isdir(".terraform")
+        if not tf_folder_already_exists:
+            terraform_init()
+
+        terraform_provider = get_terraform_provider()
+
+        if terraform_provider:
+            metadata_name = "module-metadata.json"
+            run_metadata_generator(metadata_name, terraform_provider)
+        else:
+            print("Error: Terraform provider does not exists.")
+            sys.exit(1)
+
+        if not tf_folder_already_exists:
+            remove_terraform_folder()
+
+
+main()
