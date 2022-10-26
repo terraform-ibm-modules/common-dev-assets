372b32514cba291ed252327d02ef75bf2ea41c27
Andrej <akocbek@ie.ibm.com>
2022-10-26 10:23:10 +0100

    update metadata module generator

--- module-assets/ci/terraformConfigInspect.py
+++ module-assets/ci/terraformConfigInspect.py
@@ -4,10 +4,23 @@ import glob
 import os
 import sys
 from pathlib import Path
+from subprocess import PIPE, Popen
 
 
 def terraform_init():
-    os.system("terraform init -upgrade")
+    tf_init_command = "terraform init"
+    proc = Popen(tf_init_command, stdout=PIPE, stderr=PIPE, shell=True)
+    proc.communicate()
+    return proc.returncode
+
+
+def terraform_init_upgrade():
+    tf_init_upgrade_command = "terraform init --upgrade"
+    proc = Popen(tf_init_upgrade_command, stdout=PIPE, stderr=PIPE, shell=True)
+    error = proc.communicate()
+    if proc.returncode != 0:
+        print(error)
+        sys.exit(proc.returncode)
 
 
 def get_terraform_provider():
@@ -24,11 +37,17 @@ def run_metadata_generator(file_path, terrraform_provider):
 
 def main():
     if glob.glob("*.tf"):
+        # always run terraform init
+        error_code = terraform_init()
 
-        terraform_init()
+        # if error then try terraform init upgrade
+        if error_code != 0:
+            terraform_init_upgrade()
 
+        #  get IBM terraform provider
         terraform_provider = get_terraform_provider()
 
+        # run metadata generator tool
         if terraform_provider:
             metadata_name = "module-metadata.json"
             run_metadata_generator(metadata_name, terraform_provider)
