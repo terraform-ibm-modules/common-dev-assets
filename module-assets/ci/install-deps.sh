2164bdd3eb2e69aa6ceb359abdc2a91a7c498d48
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-12-08 18:13:53 +0000

    fix(deps): update dependency kubernetes/kubernetes to v1.25.5 (#273)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -432,7 +432,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=kubernetes/kubernetes
-KUBECTL_VERSION=v1.25.4
+KUBECTL_VERSION=v1.25.5
 BINARY=kubectl
 set +e
 INSTALLED_KUBECTL_VERSION="$(kubectl version --output yaml --client | grep "gitVersion" | cut -d' ' -f4)"
