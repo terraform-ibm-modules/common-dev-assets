1017193a466e3705477c61d21bb461aaf706a21d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-12-26 18:14:28 +0000

    chore(deps): update all (#322)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -265,7 +265,7 @@ fi
 # tflint
 #######################################
  # renovate: datasource=github-releases depName=terraform-linters/tflint
-TFLINT_VERSION=v0.43.0
+TFLINT_VERSION=v0.44.0
 BINARY=tflint
 set +e
 INSTALLED_TFLINT_VERSION="$(tflint --version | grep "TFLint version " |cut -d' ' -f3)"
