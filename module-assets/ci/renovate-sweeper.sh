c60dbada147570667df575972cfe721a83bcb853
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-01-30 10:42:55 +0000

    Update renovate-sweeper.sh

--- module-assets/ci/renovate-sweeper.sh
+++ module-assets/ci/renovate-sweeper.sh
@@ -26,6 +26,7 @@ function git_push() {
 
 # Determine if PR
 IS_PR=false
+git -v
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
   # GITHUB_HEAD_REF: This property is only set when the event that triggers a workflow run is either pull_request or pull_request_target
   if [ -n "${GITHUB_HEAD_REF}" ]; then
