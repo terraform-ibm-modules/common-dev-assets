c2465e6083b33f8b29fe6fe9f769eeaab4369b9f
Kierra <kierra.searle@ibm.com>
2023-01-31 17:14:29 -0700

    fix: license pre-commit hook updated with correct functionality

--- module-assets/ci/check_for_license.sh
+++ module-assets/ci/check_for_license.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # Pre-commit hook that checks certain files exist in the project
-# first argument must be --arg1={the number of files you want to check exist}
+# first argument must be --NUM_OF_FILES_TO_CHECK={the number of files you want to check exist}
 
 v="${1/--/}"
-declare $v
+declare "$v"
 
-max=$(($arg1+2))
+max=$((NUM_OF_FILES_TO_CHECK+2))
 
 # create an array of all the files
 array=()
 index=0
-for ((x=$max; x<=$#; x++))
+for ((x=max; x<=$#; x++))
 do
     array[$index]=${!x}
-    index=$(($index+1))
+    index=$((index+1))
 done
 
 # check that all files specified exist in root of project
-for ((i=2; i<$max; i++))
+for ((i=2; i<max; i++))
 do
-    if [[ ! " ${array[*]} " =~ " ${!i} " ]]; then
+    if [[ ! " ${array[*]} " =~ ${!i} ]]; then
         echo "${!i} missing from project. Please correct before continuing."
         exit 1
     fi
