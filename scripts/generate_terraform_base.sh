bb841131d9a6d589022d64294e5445f4fa743cf3
Daniel Butler <dannyeb@gmail.com>
2023-02-09 14:57:00 +0000

    feat: do not override existing files
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- scripts/generate_terraform_base.sh
+++ scripts/generate_terraform_base.sh
@@ -1,42 +1,72 @@
 #!/bin/bash
-# Generates the standard terraform files for a module or submodule.
+# Generates the standard Terraform files if they do not already exist.
 
-echo "Generating the following files: main.tf, variables.tf, outputs.tf, provider.tf, version.tf, README.md"
+echo "Generating the following Terraform files: main.tf variables.tf outputs.tf provider.tf version.tf README.md"
 
-cat << EOF > main.tf
+if [ ! -f "main.tf" ]; then
+  echo "  - main.tf"
+  cat << EOF > "main.tf"
 ##############################################################################
 # Main Configuration
 ##############################################################################
 EOF
+else
+  echo "  - main.tf already exists, skipping creation"
+fi
 
-cat << EOF > variables.tf
+if [ ! -f "variables.tf" ]; then
+  echo "  - variables.tf"
+  cat << EOF > "variables.tf"
 ##############################################################################
 # Input Variables
 ##############################################################################
 EOF
+else
+  echo "  - variables.tf already exists, skipping creation"
+fi
 
-cat << EOF > outputs.tf
+if [ ! -f "outputs.tf" ]; then
+  echo "  - outputs.tf"
+  cat << EOF > "outputs.tf"
 ##############################################################################
 # Outputs
 ##############################################################################
 EOF
+else
+  echo "  - outputs.tf already exists, skipping creation"
+fi
 
-cat << EOF > provider.tf
+if [ ! -f "provider.tf" ]; then
+  echo "  - provider.tf"
+  cat << EOF > "provider.tf"
 ##############################################################################
 # Provider
 ##############################################################################
 EOF
+else
+  echo "  - provider.tf already exists, skipping creation"
+fi
 
-cat << EOF > version.tf
+if [ ! -f "version.tf" ]; then
+  echo "  - version.tf"
+  cat << EOF > "version.tf"
 ##############################################################################
 # Terraform Version
 ##############################################################################
 EOF
+else
+  echo "  - version.tf already exists, skipping creation"
+fi
 
-cat << EOF > README.md
+if [ ! -f "README.md" ]; then
+  echo "  - README.md"
+  cat << EOF > "README.md"
 # Module
 
 This is the documentation for the Terraform module.
 EOF
+else
+  echo "  - README.md already exists, skipping creation"
+fi
 
 echo "Complete"
