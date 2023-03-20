62b9aed1f4ff11c89b5945ff6586cb1c1b4e85ef
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-03-20 12:26:08 +0000

    Update license_checker.sh

--- module-assets/ci/license_checker.sh
+++ module-assets/ci/license_checker.sh
@@ -2,17 +2,16 @@
 
 # Pre-commit hook that checks if the license exists in the project with terraform files
 
-# Won't run if repo is internal
+# exit 0 if internal repo
 if git remote -v | head -n 1 | grep -q "github.ibm"; then
-    exit 0
+  exit 0
 fi
 
-count=$(find ./*.tf 2>/dev/null | wc -l)
+# exit 0 if no terraform files found in root directory
+count=$(find ./*.tf 2>/dev/null | wc -l | xargs)
 if [ "$count" != 0 ]; then
-  exit 1
-fi
-
-if [[ ! -f "LICENSE" ]]; then
-    echo "Required file LICENSE is missing."
+  if [[ ! -f "LICENSE" ]]; then
+    echo "Required file LICENSE is missing. Please add it and try again."
     exit 1
+  fi
 fi
