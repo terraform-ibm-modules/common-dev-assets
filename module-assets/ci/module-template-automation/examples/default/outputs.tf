19322445a17cc60b6709e7e2633cdd9331441b3d
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-03-24 11:00:04 +0000

    fix: update example

--- module-assets/ci/module-template-automation/examples/default/outputs.tf
+++ module-assets/ci/module-template-automation/examples/default/outputs.tf
@@ -2,11 +2,6 @@
 # Outputs
 ##############################################################################
 
-output "vpc_id" {
-  description = "ID of VPC created"
-  value       = ibm_is_vpc.vpc.id
-}
-
 output "resource_group_name" {
   description = "Resource group name"
   value       = module.resource_group.resource_group_name
