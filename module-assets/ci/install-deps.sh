56be1f59ca5dcc7989e9b2b865710a017a262575
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-03-25 18:24:20 +0000

    fix(deps): update dependency pre-commit/pre-commit to v3.2.1 (#536)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -138,7 +138,7 @@ fi
 #######################################
 
  # renovate: datasource=github-tags depName=pre-commit/pre-commit
-PRE_COMMIT_VERSION=v3.2.0
+PRE_COMMIT_VERSION=v3.2.1
 PACKAGE=pre-commit
 set +e
 INSTALLED_PRE_COMMIT_VERSION="$(${PYTHON} -m pip show pre-commit | grep Version: | cut -d' ' -f2)"
