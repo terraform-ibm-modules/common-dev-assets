4a75955bf48717b6f092b8e3c9444ea8afc631b4
mounika-nalla <121987799+mounika-nalla@users.noreply.github.com>
2023-06-26 09:08:01 -0400

    fix: update src script (#647)
    * fix: update src script
    
    * feat: update hashicorp refs
    
    * fix: GE regex
    
    ---------
    
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- module-assets/ci/module-template-automation/examples/default/main.tf
+++ module-assets/ci/module-template-automation/examples/default/main.tf
@@ -3,7 +3,8 @@
 ##############################################################################
 
 module "resource_group" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.0.5"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
