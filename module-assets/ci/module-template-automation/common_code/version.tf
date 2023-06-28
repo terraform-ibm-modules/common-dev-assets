9f69b19c9ca0d738463c38b176d833d9323787be
ocofaigh <ocofaigh@ie.ibm.com>
2023-06-28 11:26:35 +0100

    docs: final clean

--- module-assets/ci/module-template-automation/common_code/version.tf
+++ module-assets/ci/module-template-automation/common_code/version.tf
@@ -1,11 +1,12 @@
 terraform {
-  required_version = ">= 1.0.0"
-  # Add any required providers below and uncomment
+  # Since hashicorp can include breaking changes in minor version updates, ensure to lock into a range here.
+  required_version = ">= 1.3, < 1.6"
+
+  # Use a flexible range in modules that future proofs the module's usage with upcoming minor and patch versions
   #  required_providers {
   #    ibm = {
-  #      source = "IBM-Cloud/ibm"
-  #      # Use "greater than or equal to" range in modules
-  #      version = ">= 1.45.0"
+  #      source  = "IBM-Cloud/ibm"
+  #      version = ">= 1.49.0, < 2.0.0"
   #    }
   #  }
 }
--- module-assets/ci/module-template-automation/examples/basic/README.md
+++ module-assets/ci/module-template-automation/examples/basic/README.md
@@ -3,6 +3,6 @@
 <!-- There is a pre-commit hook that will take the title of each example add include it in the repos main README.md  -->
 <!-- The text below should describe exactly what resources are provisioned / configured by the example  -->
 
-An end-to-end basic example that uses the IBM Cloud terraform provider to
-- Create a new resource group if one is not passed in.
-- Create a new Cloud Object Storage instance.
+An end-to-end basic example that will provision the following:
+- A new resource group if one is not passed in.
+- A new Cloud Object Storage instance.
