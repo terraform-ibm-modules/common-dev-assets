e735fa7b4c19127c7529ba93c7e7b7e0bcb96026
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-07-22 04:03:16 +0100

    fix(deps): update terraform terraform-ibm-modules/resource-group/ibm to v1.0.6 (#691)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- module-assets/ci/module-template-automation/examples/basic/main.tf
+++ module-assets/ci/module-template-automation/examples/basic/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.0.5"
+  version = "1.0.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
