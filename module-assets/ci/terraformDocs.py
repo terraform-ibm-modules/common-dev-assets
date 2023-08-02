2be3daa1fb76f565fa6724437866188ba96a31c9
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-08-02 10:55:17 +0100

    Revert "fix: fix so that custom terradocs hook runs against all markdown files with the metadata (#703)" (#704)
    This reverts commit 4ed5c313585279293e677a9b5bc989f92db58992.

--- module-assets/ci/terraformDocs.py
+++ module-assets/ci/terraformDocs.py
@@ -6,40 +6,30 @@ from subprocess import PIPE, Popen
 
 import terraformDocsUtils
 
-# temp file
-markdown = "tf-docs.md"
+if terraformDocsUtils.is_hook_exists(
+    "<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->"
+):
+    # temp file
+    markdown = "tf-docs.md"
 
-for arg in sys.argv:
-    if arg == sys.argv[0]:
-        continue
-    else:
-        if terraformDocsUtils.is_hook_exists(
-            "<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->", arg
-        ):
-            # get terraform docs content
-            terraformDocsUtils.create_markdown("", markdown)
-            get_tf_docs_content_command = (
-                "terraform-docs --hide providers markdown table ."
-            )
-            proc = Popen(
-                get_tf_docs_content_command, stdout=PIPE, stderr=PIPE, shell=True
-            )
-            output, error = proc.communicate()
+    # get terraform docs content
+    get_tf_docs_content_command = "terraform-docs --hide providers markdown table ."
+    proc = Popen(get_tf_docs_content_command, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = proc.communicate()
 
-            # store tf docs content into temp file and change headings from lvl 2 to lvl 3
-            with open(markdown, "w") as writer:
-                writer.write(output.decode("utf-8").strip().replace("##", "###"))
+    # store tf docs content into temp file and change headings from lvl 2 to lvl 3
+    with open(markdown, "w") as writer:
+        writer.write(output.decode("utf-8").strip().replace("##", "###"))
 
-            if proc.returncode != 0:
-                print(error)
-                terraformDocsUtils.remove_markdown(markdown)
-                sys.exit(proc.returncode)
+    if proc.returncode != 0:
+        print(error)
+        sys.exit(proc.returncode)
 
-            # add tf-docs.md content to main README
-            os.system(
-                f"terraform-docs --output-file {arg} -c common-dev-assets/module-assets/.terraform-docs-config.yaml ."
-            )
+    # add tf-docs.md content to main README
+    os.system(
+        "terraform-docs -c common-dev-assets/module-assets/.terraform-docs-config.yaml ."
+    )
 
-            # remove temp file
-            if os.path.exists(markdown):
-                os.remove(markdown)
+    # remove temp file
+    if os.path.exists(markdown):
+        os.remove(markdown)
