08f01551fe40c56cbada9d00005e675ed9e12161
ocofaigh <ocofaigh@ie.ibm.com>
2023-08-10 18:08:34 +0100

    ci: remove mod tem hook

--- module-assets/ci/module-template-automation/common_code/variables.tf
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# Input Variables
-##############################################################################
-
-#variable "my_variable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = "default_value"
-#}
