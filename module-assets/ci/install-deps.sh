de6cf777de24a10d01e9a6cd5d61a232ed009ae8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-09-14 08:21:49 +0000

    fix(deps): update dependency kubernetes/kubernetes to v1.28.2

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -457,7 +457,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=kubernetes/kubernetes
-KUBECTL_VERSION=v1.28.1
+KUBECTL_VERSION=v1.28.2
 BINARY=kubectl
 set +e
 INSTALLED_KUBECTL_VERSION="$(kubectl version --output yaml --client | grep "gitVersion" | cut -d' ' -f4)"
