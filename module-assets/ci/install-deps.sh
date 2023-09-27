6be04d4f8e386a67f95c8316daf134db54c98171
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-09-27 10:12:16 +0000

    fix(deps): update dependency gruntwork-io/terragrunt to v0.51.5

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -235,7 +235,7 @@ clean "${TMP_DIR}"
 #######################################
 
  # renovate: datasource=github-releases depName=gruntwork-io/terragrunt
-TERRAGRUNT_VERSION=v0.51.4
+TERRAGRUNT_VERSION=v0.51.5
 BINARY=terragrunt
 set +e
 INSTALLED_TERRAGRUNT_VERSION="$(terragrunt --version | head -1 | cut -d' ' -f3)"
