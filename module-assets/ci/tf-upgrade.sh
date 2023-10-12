c263cc47478ff4b20e32d886796cfcdc9bd4ac4b
Andrej <akocbek@ie.ibm.com>
2023-10-12 11:43:15 +0100

    feat: add hook to upgrade terraform inside examples

--- module-assets/ci/tf-upgrade.sh
+++ module-assets/ci/tf-upgrade.sh
@@ -2,15 +2,15 @@
 set -e
 
 BASE_DIR=$(pwd)
-search_dir=./examples
+examples_dir=./examples
 # run code for each example
-for entry in "$search_dir"/*
+for example in "$examples_dir"/*
 do
-    cd $entry
+    cd "$example"
     TERRAFORM_DIR=.terraform
     # if .terraform folder exists then run terraform upgrade
     if [ -d "$TERRAFORM_DIR" ]; then
         terraform init -upgrade
     fi
-    cd $BASE_DIR
+    cd "$BASE_DIR"
 done
