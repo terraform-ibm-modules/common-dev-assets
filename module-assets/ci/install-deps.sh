a3e648d6fe2fc8caaad73dbf6e6c51ba20f05d79
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-10-13 07:39:48 +0000

    fix(deps): update all

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -242,7 +242,7 @@ clean "${TMP_DIR}"
 #######################################
 
  # renovate: datasource=github-releases depName=gruntwork-io/terragrunt
-TERRAGRUNT_VERSION=v0.52.1
+TERRAGRUNT_VERSION=v0.52.2
 BINARY=terragrunt
 set +e
 INSTALLED_TERRAGRUNT_VERSION="$(terragrunt --version | head -1 | cut -d' ' -f3)"
@@ -437,7 +437,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=helm/helm
-HELM_VERSION=v3.13.0
+HELM_VERSION=v3.13.1
 BINARY=helm
 set +e
 INSTALLED_HELM_VERSION="$(helm version | cut -d':' -f2 | cut -d'"' -f2)"
