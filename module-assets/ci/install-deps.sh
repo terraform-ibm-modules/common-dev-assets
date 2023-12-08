5552dcb0013e3d1e22cdcf7a2563c0a941988090
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-12-08 20:15:51 +0000

    fix(deps): update all

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -166,7 +166,7 @@ fi
 #######################################
 
  # renovate: datasource=github-tags depName=ibm/detect-secrets versioning="regex:^(?<compatibility>.*)-?(?<major>\\d+)\\.(?<minor>\\d+)\\+ibm\\.(?<patch>\\d+)\\.dss$"
-DETECT_SECRETS_VERSION=0.13.1+ibm.61.dss
+DETECT_SECRETS_VERSION=0.13.1+ibm.62.dss
 PACKAGE=detect-secrets
 set +e
 INSTALLED_DECTECT_SECRETS="$(${PYTHON} -m pip show detect-secrets | grep Version: | cut -d' ' -f2)"
@@ -242,7 +242,7 @@ clean "${TMP_DIR}"
 #######################################
 
  # renovate: datasource=github-releases depName=gruntwork-io/terragrunt
-TERRAGRUNT_VERSION=v0.53.8
+TERRAGRUNT_VERSION=v0.54.0
 BINARY=terragrunt
 set +e
 INSTALLED_TERRAGRUNT_VERSION="$(terragrunt --version | head -1 | cut -d' ' -f3)"
