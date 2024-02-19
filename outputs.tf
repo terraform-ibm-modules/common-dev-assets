3bc0a46e5494a48251f2a1e111d9b66e48944e95
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-02-19 09:04:07 +0000

    [NO MERGE] test

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+##############################################################################
+# Outputs
+##############################################################################
+
+output "id" {
+  description = "Event Streams instance id"
+  value       = "ibm_resource_instance.es_instance.id"
+}
