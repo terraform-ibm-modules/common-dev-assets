91530fddc5582ca5d9709b352176fe079792ea3d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-03-29 20:17:59 +0000

    fix(deps): update all

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -341,7 +341,7 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 fi
 
 # renovate: datasource=github-releases depName=aquasecurity/trivy
-TRIVY_VERSION=v0.50.0
+TRIVY_VERSION=v0.50.1
 BINARY=trivy
 set +e
 INSTALLED_TRIVY_VERSION="$(trivy version | grep "Version:" | cut -d' ' -f2)"
@@ -369,7 +369,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION=v1.57.1
+GOLANGCI_LINT_VERSION=v1.57.2
 BINARY=golangci-lint
 set +e
 INSTALLED_GOLANGCI_LINT_VERSION="$(golangci-lint --version | head -1 | cut -d' ' -f4)"
