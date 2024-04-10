cf5ecba4959947fb63ace764fbb6f31866ee7908
Daniel Butler <dannyeb@gmail.com>
2024-04-10 14:23:28 +0100

    update subtree script
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- module-assets/ci/update_subtree.sh
+++ module-assets/ci/update_subtree.sh
@@ -3,11 +3,12 @@
 # Update the common-dev-assets subtree
 git subtree pull --prefix common-dev-assets https://github.com/terraform-ibm-modules/common-dev-assets stacks --squash
 
-# Add subtree changes
-git add common-dev-assets
-
-# if any changes exit with error code 1
-if [ -n "$(git status --porcelain)" ]; then
+# Check if the subtree update made any changes
+if git diff-index --quiet HEAD --; then
+  # No changes made by the subtree update
+  exit 0
+else
+  # Changes made by the subtree update
   echo "common-dev-assets subtree has changes. Please run pre-commit again"
   exit 1
 fi
