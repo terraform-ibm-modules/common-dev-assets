d85e954e0feaf9ba2eba9470c38c65635e365843
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-04-12 19:17:30 +0000

    fix(deps): update all

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -452,7 +452,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=helm/helm
-HELM_VERSION=v3.14.3
+HELM_VERSION=v3.14.4
 BINARY=helm
 set +e
 INSTALLED_HELM_VERSION="$(helm version | cut -d':' -f2 | cut -d'"' -f2)"
