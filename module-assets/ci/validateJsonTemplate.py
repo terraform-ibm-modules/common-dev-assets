788c7594a6185236855279d3db31bfc99940e9b4
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-05-23 14:13:34 +0100

    ci: update json_template_lint pre-commit hook to support Stack

--- module-assets/ci/validateJsonTemplate.py
+++ module-assets/ci/validateJsonTemplate.py
@@ -32,7 +32,7 @@ def is_json(myjson):
         return True
 
 
-# using terraform-docs to get all tf inputs
+# create 'temp_tf_inputs.json' file using terraform-docs to get all tf inputs
 def create_tf_input_json(root, output_file):
     # get tf inputs
     command = f'terraform-docs -c common-dev-assets/module-assets/.terraform-docs-config-validate-json-template.yaml --output-file {output_file} "{root}" '
@@ -45,58 +45,98 @@ def create_tf_input_json(root, output_file):
         sys.exit(proc.returncode)
 
 
-# validate catalogValidationValues.json.template keys
-def validate_inputs(root, temp_catalog_template_file, original_catalog_template_file):
+# get terraform inputs from 'variables.tf'
+def get_tf_inputs_with_tf_docs(root):
+    tf_inputs_name = []
     temp_tf_inputs_json_file = "temp_tf_inputs.json"
     create_tf_input_json(root, temp_tf_inputs_json_file)
 
-    catalog_template_keys = []
-    with open(temp_catalog_template_file) as json_catalog_template_data:
-        data = json.load(json_catalog_template_data)
-        catalog_template_keys = data.keys()
-
-    tf_inputs_name = []
     with open(os.path.join(root, temp_tf_inputs_json_file)) as json_data:
         data = json.load(json_data)
         for tf_input in data["inputs"]:
             tf_inputs_name.append(tf_input["name"])
 
-    # check if catalog_template key is part of terraform input variables of the same directory
-    for catalog_template_key in catalog_template_keys:
-        if catalog_template_key not in tf_inputs_name:
-            validation_errors.append(
-                f"'{catalog_template_key}' defined in '{original_catalog_template_file}' is not a valid input variable for the terraform in this directory."
-            )
-
-    os.remove(
-        os.path.join(root, temp_tf_inputs_json_file)
-    )  # remove temp temp_tf_inputs.json file
+    # remove temp temp_tf_inputs.json file
+    os.remove(os.path.join(root, temp_tf_inputs_json_file))
+    return tf_inputs_name
 
 
-# find all 'catalogValidationValues.json.template' files in 'solutions' folder
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file.endswith("catalogValidationValues.json.template"):
-            if ".terraform" not in os.path.join(root, file):
+# get terraform inputs from 'stack_definition.json' file
+def get_tf_inputs_from_stack_definition(root, stack_definition_json_file):
+    tf_inputs_name = []
 
-                # create a new temp catalogValidationValues.json.template file
-                original_catalog_file = os.path.join(root, file)
-                temp_catalog_file = create_temp_json(root, file)
+    with open(os.path.join(root, stack_definition_json_file)) as json_data:
+        data = json.load(json_data)
+        for tf_input in data["inputs"]:
+            tf_inputs_name.append(tf_input["name"])
+    return tf_inputs_name
 
-                # if catalogValidationValues.json.template is not valid JSON format then save the error
-                if not is_json(temp_catalog_file):
-                    validation_errors.append(
-                        original_catalog_file + " is not valid JSON format."
-                    )
-                else:
-                    validate_inputs(root, temp_catalog_file, original_catalog_file)
 
-                os.remove(
-                    temp_catalog_file
-                )  # remove temp catalogValidationValues.json.template file
+# validate catalogValidationValues.json.template keys
+def validate_inputs(root, temp_catalog_template_file, original_catalog_template_file):
+    # terraform inputs
+    tf_inputs_name = []
+    stack_definition_json_file = "stack_definition.json"
 
-# if any validation error occurs then print the error and fail the hook
-if validation_errors:
-    for error in validation_errors:
-        print(error)
-    sys.exit(1)
+    # if 'variables.tf' file exists then get the terraform inputs using terraform-docs
+    if any(File == "variables.tf" for File in os.listdir(root)):
+        tf_inputs_name = get_tf_inputs_with_tf_docs(root)
+    # if 'variables.tf' file does not exist then get the terraform inputs from 'stack_definition.json' file (Stack case)
+    elif any(File == stack_definition_json_file for File in os.listdir(root)):
+        tf_inputs_name = get_tf_inputs_from_stack_definition(
+            root, stack_definition_json_file
+        )
+    # if 'variables.tf' and 'stack_definition.json' files do not exist then add validation error
+    else:
+        validation_errors.append(
+            f"'catalogValidationValues.json.template' shouldn't exists in '{root}' if 'variables.tf' or 'stack_definition.json' are not in this directory."
+        )
+
+    # validate only if terraform inputs exist
+    if tf_inputs_name:
+        # get catalog template keys
+        catalog_template_keys = []
+        with open(temp_catalog_template_file) as json_catalog_template_data:
+            data = json.load(json_catalog_template_data)
+            catalog_template_keys = data.keys()
+
+        # check if catalog_template key is part of terraform input variables of the same directory
+        for catalog_template_key in catalog_template_keys:
+            if catalog_template_key not in tf_inputs_name:
+                validation_errors.append(
+                    f"'{catalog_template_key}' defined in '{original_catalog_template_file}' is not a valid input variable for the terraform in this directory."
+                )
+
+
+def main():
+    # find all 'catalogValidationValues.json.template' files in 'solutions' folder
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith("catalogValidationValues.json.template"):
+                if ".terraform" not in os.path.join(root, file):
+
+                    # create a new temp catalogValidationValues.json.template file
+                    original_catalog_file = os.path.join(root, file)
+                    temp_catalog_file = create_temp_json(root, file)
+
+                    # if catalogValidationValues.json.template is not valid JSON format then save the error
+                    if not is_json(temp_catalog_file):
+                        validation_errors.append(
+                            original_catalog_file + " is not valid JSON format."
+                        )
+                    else:
+                        validate_inputs(root, temp_catalog_file, original_catalog_file)
+
+                    os.remove(
+                        temp_catalog_file
+                    )  # remove temp catalogValidationValues.json.template file
+
+    # if any validation error occurs then print the error and fail the hook
+    if validation_errors:
+        for error in validation_errors:
+            print(error)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
