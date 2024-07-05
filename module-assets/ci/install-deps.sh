12c9e98e9c8ff9dd984431d10760802596417ba6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-07-05 23:11:49 +0000

    fix(deps): update dependency warrensbox/terraform-switcher to v1.2.0

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -190,7 +190,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=warrensbox/terraform-switcher
-TFSWITCH_VERSION=v1.1.1
+TFSWITCH_VERSION=v1.2.0
 BINARY=tfswitch
 set +e
 INSTALLED_TFSWITCH_VERSION="$(tfswitch --version | grep Version | awk '{ print $2 }')"
