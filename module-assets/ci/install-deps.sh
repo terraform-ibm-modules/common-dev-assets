701641aa162ed15594181cc33726998b259cd4a1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-17 15:10:49 +0000

    fix(deps): update dependency terraform-linters/tflint to v0.53.0

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -304,7 +304,7 @@ fi
 # tflint
 #######################################
  # renovate: datasource=github-releases depName=terraform-linters/tflint
-TFLINT_VERSION=v0.52.0
+TFLINT_VERSION=v0.53.0
 BINARY=tflint
 set +e
 INSTALLED_TFLINT_VERSION="$(tflint --version | grep "TFLint version " |cut -d' ' -f3)"
