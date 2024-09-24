1ad5ce0695e06e4947638496acfe371d363f80f1
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-09-24 14:39:07 +0100

    Update run-tests.sh

--- module-assets/ci/run-tests.sh
+++ module-assets/ci/run-tests.sh
@@ -63,7 +63,7 @@ if [ ${IS_PR} == true ]; then
 
   # Files that should not trigger tests
   # NOTE: We are purposely running tests in PRs with 'common-dev-assets' GIT submodule updates since
-  # terraform version can change, and we will want to run full set of tests if that happens)
+  # terraform version can change, and we will want to run full set of tests if that happens.
   declare -a skip_array=(".drawio"
                          ".github/settings.yml"
                          ".github/workflows/ci.yml"
