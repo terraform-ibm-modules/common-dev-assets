ee1ca8f06ae836b401a6fbd70dfee542b6988d7f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-11-10 22:51:48 +0000

    fix(deps): update dependency golangci/golangci-lint to v1.62.0 (#1065)

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -341,7 +341,7 @@ fi
 #######################################
 
  # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION=v1.61.0
+GOLANGCI_LINT_VERSION=v1.62.0
 BINARY=golangci-lint
 set +e
 INSTALLED_GOLANGCI_LINT_VERSION="$(golangci-lint --version | head -1 | cut -d' ' -f4)"
