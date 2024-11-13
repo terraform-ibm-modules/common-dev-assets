407ccf03324ca4e2189c5dfe13eaa17d9d800fdd
Arya Girish K <arya.girish.k@ibm.com>
2024-11-13 06:52:57 +0000

    feat : detect ibm_catalog.json

--- module-assets/ci/license_checker.sh
+++ module-assets/ci/license_checker.sh
@@ -8,14 +8,12 @@ if git remote -v | head -n 1 | grep -q "github.ibm"; then
 fi
 
 
-# ensure LICENSE file exists if .tf file is detected in root directory
+# ensure LICENSE file exists if .tf file is detected incd root directory
 
-count=$(find ./ -type f \( -name *.tf -o -name ibm_catalog.json \) 2>/dev/null | wc -l | xargs)
+count=$(find . -maxdepth 1 \( -name "*.tf" -o -name "ibm_catalog.json" \) 2>/dev/null | wc -l | xargs)
 if [ "$count" != 0 ]; then
   if [[ ! -f "LICENSE" ]]; then
     echo "Required LICENSE file is missing. Please add it and try again."
     exit 1
   fi
 fi
-
-  
