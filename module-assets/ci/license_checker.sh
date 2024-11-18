e5849d95c6a8ef56a002e0a2b50b84766786de1e
Arya Girish K <arya.girish.k@ibm.com>
2024-11-18 18:01:56 +0530

    updated PR

--- module-assets/ci/license_checker.sh
+++ module-assets/ci/license_checker.sh
@@ -9,10 +9,7 @@ fi
 
 
 # ensure LICENSE file exists if .tf file or ibm_catalog.json is detected in root directory
-
-var1=$(find ./*.tf 2>/dev/null | wc -l | xargs)
-var2=$(find ibm_catalog.json 2>/dev/null | wc -l | xargs)
-count=$((var1+var2))
+count=$(find . -type f \( -name "*.tf" -o -name "ibm_catalog.json" \) | grep -vc "^./common-dev-assets/")
 if [ "$count" != 0 ]; then
   if [[ ! -f "LICENSE" ]]; then
     echo "Required LICENSE file is missing. Please add it and try again."
