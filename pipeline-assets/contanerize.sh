fc76331aac7c71780a3ff32402edbd31050b4086
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-07-24 15:56:26 +0100

    add support for prisma scan (#1196)

--- pipeline-assets/contanerize.sh
+++ pipeline-assets/contanerize.sh
@@ -13,6 +13,8 @@ export ARTIFACTORY_USERNAME="$(get_env artifactory-user)"
 export ARTIFACTORY_PASSWORD="$(get_env artifactory-password)"
 # shellcheck disable=SC2155
 export IBMCLOUD_APIKEY="$(get_env ibmcloud-api-key)"
+# shellcheck disable=SC2155
+export SCAN_ENGINES="$(get_env image-scan-engines ibm_va)" # default to 'ibm_va' but also supports 'ibm_va,prisma_cloud' if whitelisted for Prisma scanning
 
 # build the image
 make docker-build || exit_code=$?
@@ -41,7 +43,7 @@ if [[ "${exit_code}" -eq 0 ]]; then
     REGISTRY="$(get_env 'DEV_REPO'  | cut -f1 -d/  )"
     NAMESPACE="$(get_env 'DEV_REPO' | cut -f2 -d/ )"
 
-    ./common-dev-assets/pipeline-assets/image_vuln_scan.sh "${REGISTRY}" "${NAMESPACE}" "${IMAGE_NAME}" "${IMAGE_TAG}" "${IBMCLOUD_APIKEY}" false  || exit_code=$?
+    ./common-dev-assets/pipeline-assets/image_vuln_scan.sh "${REGISTRY}" "${NAMESPACE}" "${IMAGE_NAME}" "${IMAGE_TAG}" "${IBMCLOUD_APIKEY}" false "${SCAN_ENGINES}"  || exit_code=$?
   fi
 else
   echo "Skipping push to ICR and VA scan due to earlier failure"
