29b7b43c9dd3183bbfcde228929b9e5f64b33f2a
Akash Kumar <Ak-sky@users.noreply.github.com>
2025-11-14 19:02:21 +0530

    fix: Fix sch btn for forked PRs (#1277)

--- module-assets/ci/terraformDocOverview.py
+++ module-assets/ci/terraformDocOverview.py
@@ -5,6 +5,7 @@ import pathlib
 import re
 from pathlib import Path
 
+import terraformDocGoMod
 import terraformDocsUtils
 
 
@@ -64,14 +65,20 @@ def get_main_readme_headings():
 
 def get_repo_info():
     module_url = terraformDocsUtils.get_module_url()
+
+    module_url = terraformDocGoMod.change_module_url(module_url)
+
     full_module_url = f"https://{module_url}"
+
     repo_name = pathlib.PurePath(module_url).name
     module_name = repo_name.replace("terraform-ibm-", "")
     words = module_name.split("-")
+
     if len(words) == 1:
         short_name = module_name
     else:
         short_name = "".join([word[0] for word in words])
+
     return full_module_url, short_name
 
 
