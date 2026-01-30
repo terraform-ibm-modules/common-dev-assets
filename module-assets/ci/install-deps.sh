2cc81b570cc55dd5323c1cfc48f444f9beede521
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-30 20:17:47 +0000

    fix(deps): update dependencies

--- module-assets/ci/install-deps.sh
+++ module-assets/ci/install-deps.sh
@@ -396,7 +396,7 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 fi
 
 # renovate: datasource=github-releases depName=aquasecurity/trivy
-TRIVY_VERSION=v0.68.2
+TRIVY_VERSION=v0.69.0
 BINARY=trivy
 set +e
 INSTALLED_TRIVY_VERSION="$(trivy version | grep "Version:" | cut -d' ' -f2)"
